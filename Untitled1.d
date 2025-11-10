bc6720c758c1bdfe4f7c4fbf370ef90ec88aa9ee
Mehedi-hub-maker <253-16-069@diu.edu.bd>
2025-11-10 23:20:37 +0600

    Add files via upload

---
+++ Untitled1.d
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,max;
+    printf("enter any number :");
+    scanf("%d %d",&a,&b);
+    if(a>b)
+    max=a;
+    else
+    max=b;
+    printf("maximum = %d",max);
+
+    }
